b54878da20cdd59d7bd1fd26d4ed43e2e7733687
Jurica Kenda <jurica.kenda@gmail.com>
2019-04-03 17:15:45 +0200

    Importing Maven module from SpringInitializr

---
+++ RESTFulDatabaseAccess/src/main/java/com/restfuldatabaseaccess/RESTFulDatabaseAccess/RestFulDatabaseAccessApplication.java
@@ -0,0 +1,13 @@
+package com.restfuldatabaseaccess.RESTFulDatabaseAccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulDatabaseAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulDatabaseAccessApplication.class, args);
+	}
+
+}
