b71b2a7a00d6a104ba36da9bea8e7e4c5f4b09ad
Jurica Kenda <jurica.kenda@gmail.com>
2019-05-09 23:35:39 +0200

    Configured the POST and GET requests to send data between templates

--- RESTFulDatabaseAccess/src/main/java/com/restfuldatabaseaccess/controllers/HtmlResponseController.java
+++ RESTFulDatabaseAccess/src/main/java/com/restfuldatabaseaccess/controllers/HtmlResponseController.java
@@ -1,10 +1,16 @@
 package com.restfuldatabaseaccess.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.restfuldatabaseaccess.entities.Student;
 import com.restfuldatabaseaccess.entities.StudentRepo;
 
 @Controller
@@ -20,11 +26,23 @@ public class HtmlResponseController {
 	}
 	
 	
-	@RequestMapping(value="/table")
+	@GetMapping(value="/table")
 	public String getStudents(Model model) {
-		model.addAttribute("students",repo.getStudents());
+		model.addAttribute("studentsinfo",repo.getStudents());
+		model.addAttribute("stud", new Student());
 		return "students";
 	}
+	
+	@PostMapping(value="/table")
+	public String getStudents(Model model,@ModelAttribute Student student) {
+		List<Student> filtered = repo.getStudents()
+									 .stream()
+									 .filter(x -> x.getIme().equals(student.getIme()))
+									 .collect(Collectors.toList());
+		model.addAttribute("studentsinfo",filtered);
+		return "studentsfiltered";
+	}
+	
 	
 	
 }
